5d1934963987f9f8a71ad8f5ddcf920fa3e35c0b
Luc Perkins <luc@appfog.com>
2012-10-23 15:50:38 -0700

    OK

--- app.rb
+++ app.rb
@@ -16,6 +16,8 @@ end
 
 get '/' do
   @title = "Sinatra + Redis + AppFog = WIN"
+
+  @time = Time.now.to_s
   
   @notes = redis.lrange("notes", 0, -1)
 
@@ -28,19 +30,25 @@ end
 
 post '/newnote' do
   if params[:newNote].length >= 1
-    @newNote = params[:newNote]
-    redis.rpush("notes", @newNote)
+    # @newNote = params[:newNote]
+    redis.rpush("notes", params[:newNote])
   end
 
   redirect '/'
 end
 
 post '/sortalpha' do
-  @notes.sort!
-
-  @notes
+  @notes = redis.sort("notes", alpha)
 
   haml :index
+end
+
+post '/deleteNote' do
+  @noteToDelete = redis.lindex("notes", params[:deleteNote])
+
+  redis.lrem("notes", @noteToDelete)
+
+  redirect '/'
 end
 
 post '/' do
---
+++ public/notes.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$('li').hover(
+		function(){
+			$(this).toggleClass('white');
+		});
+});
