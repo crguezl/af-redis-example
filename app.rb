8393f12ba028600fe4801983cce991c9d11fcb1d
Luc Perkins <luc@appfog.com>
2012-10-29 15:57:11 -0700

    OK

--- app.rb
+++ app.rb
@@ -4,17 +4,13 @@ require 'redis'
 require 'haml'
 require 'json'
 
-#configure do
- # services = JSON.parse(ENV['VCAP_SERVICES'])
- # redis_key = services.key.select { |svc| svc =~ /redis/i }.first
- # redis = services[redis_key].first['credentials']
- # redis_conf = { :host => redis['hostname'], :port => redis['port'], :password => redis['password'] }
- # @@redis = Redis.new redis_conf
-#end
-
-#redisConfig = { :port => VCAP_SERVICES }
-
-redis = Redis.new# redisConfig
+configure do
+    services = JSON.parse(ENV['VCAP_SERVICES'])
+    redis_key = services.keys.select { |svc| svc =~ /redis/i }.first
+    redis = services[redis_key].first['credentials']
+    redis_conf = { :host => redis['hostname'], :port => redis['port'], :password => redis['password'] }
+    @@redis = Redis.new redis_conf
+end
 
 before do
   @length = redis.LLEN("notes")
